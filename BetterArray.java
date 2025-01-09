caaee44121d9bba62d5ecef417793b294160934c
MaxBennett2024 <maximus.bennett@mcusd709.org>
2025-01-09 08:45:00 -0600

    Bro what is going on

---
+++ BetterArray.java
@@ -0,0 +1,74 @@
+public class BetterArray {
+    private int item;
+    private int index;
+    private static int size;
+    public int[] array;
+    public BetterArray(){
+        size = 0;
+        array = new int[size];
+    }
+    public BetterArray(int item){
+       add(item);
+    }
+    //Returns the number of elements in the list
+
+    public int size(){
+        return size;
+    }
+
+    //appends item to the end of the list; returns true
+
+    public boolean add(int item){
+        int len=0;
+        if(size==0){
+            len = 1;
+        }else if(size>0){
+
+        }else{
+            len *=2;
+        }
+        int[] result = new int[len];
+        for(int i =0;i<array.length;i++){
+            result[i] = array[i];
+        }
+        size++;
+        return true;
+    }
+
+    // Inserts item at position index, moving elements at position index and higher to 
+    // the right and adds 1 to size
+
+    public void add(int index, int item){
+        size++;
+        
+        int var = array[index];
+        array[index] = item;
+        for(int i =index+1;i<array.length;i++){
+            int temp = array[i];
+            array[i] = var;
+            var = temp;
+        }
+    }
+
+    // Returns the element at position index in the list
+
+    public int get(int index){
+        return array[index];
+    }
+
+    // replaces the element at position index with item; returns the element formerly at position index
+
+    public int set(int index, int item){
+        int temp = array[index];
+        array[index] = item;
+        return temp;
+    }
+
+    //Removes element from position index, moving elements at position index+1 and
+    //higher to the left and subtracts 1 from size; returns the element formerly at
+    //position index
+
+    public int remove(int index){
+        return 0;
+    }
+}
